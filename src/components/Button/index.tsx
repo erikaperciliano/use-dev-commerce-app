10c2ce590bb2289f699a2f0d231b237f7fe80ccb
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-10-23 22:29:30 -0300

    feat: adds Liskov Substituition Principle: adds react native interfaces

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,29 +1,23 @@
-import { CSSProperties, MouseEvent, ReactNode } from "react";
+import { ReactNode, ButtonHTMLAttributes } from "react";
 import classnames from "classnames"; // Para combinar classes dinamicamente
 import Styles from "./Button.module.css";
 
 type ButtonProps = {
-  style?: CSSProperties;
   children?: ReactNode;
   variant?: "primary" | "secondary";
   size?: "small" | "medium" | "large"; // Define diferentes tamanhos
-  onClick: (e: MouseEvent<HTMLElement>) => void; // Manipulação de click adicional
-};
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = ({
   children,
   variant = "primary",
   size = "medium",
-  onClick,
-  style,
   ...props
 }: ButtonProps) => {
 
   return (
     <button
-      style={style}
       className={classnames(Styles[variant], Styles[size])}
-      onClick={onClick}
       {...props}
     >
       {children}
